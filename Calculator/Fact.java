bbfaec394e6aef3d7292d5c1986007dbf275f62e
Aushiffer <116667039+Aushiffer@users.noreply.github.com>
2022-11-10 06:16:23 -0300

    Add files via upload

---
+++ Calculator/Fact.java
@@ -0,0 +1,32 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class Fact {
+
+    Scanner user = new Scanner(System.in);
+    
+    static long factExact(long num) {
+
+        long x = 1;
+
+        for(int i=1; i<=num; i++) {
+
+            x *= i;
+
+        }
+
+        return x;
+
+    }
+
+    protected void fact() {
+
+        System.out.println("Number:");
+        long factorial = user.nextLong();
+        System.out.println();
+
+        System.out.println("The result is: "+factExact(factorial));
+
+    }
+}
