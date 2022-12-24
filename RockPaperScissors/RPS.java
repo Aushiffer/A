f5131ab2438cbb04ad99b033c1602f72194d964d
Aushiffer <116667039+Aushiffer@users.noreply.github.com>
2022-12-24 20:47:57 -0300

    Update RPS.java

--- RockPaperScissors/RPS.java
+++ RockPaperScissors/RPS.java
@@ -1,192 +1,147 @@
-package RockPaperScissors;
+package RoPaSci;
 
-import java.util.Random;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.Random;
 
 public class RPS {
+
+    static int jChoice() {
+
+        Random rand = new Random();
+
+        return rand.nextInt(0, 3);
+
+    }
+
     public static void main(String[] args) {
-        
-        Random cpuChoice = new Random();
-        Scanner user = new Scanner(System.in);
-        ArrayList<String> cpuChoiceList = new ArrayList<String>(3);
 
-        cpuChoiceList.add("rock ");
-        cpuChoiceList.add("scissors ");
-        cpuChoiceList.add("paper");
-        
-        int userScore = 0; int javaScore = 0;
-        
-        System.out.println("Welcome to the Java version of the classic \"Rock, Paper and Scissors\" game!");
-        System.out.print("What's your name? ");
-        String username = user.nextLine();
-        
+        String[] compChoice = new String[] {"rock", "paper", "scissors"};
+
+        int pScore = 0;
+        int jScore = 0;
+
+        Scanner scan = new Scanner(System.in);
+
         while(true) {
-            
-            if(!(username.isBlank())) {
-                
-                break;
 
-            }
-            
-            if(username.isBlank()) {
-            
-            System.out.println("Please input something on the name field: ");
-            username = user.nextLine();
+            System.out.println("Rock, paper and scissors");
+            System.out.println();
 
-            }
-
-        }
-        
-        System.out.println("Hi, "+username+"!");
-        System.out.print("\n");
-        
-        while(true) {
-        System.out.println("- "+username+", please choose one of these: ");
-        
-        for(int i=0; i<cpuChoiceList.size(); i++) {
+            System.out.println("Choose one of them: ");
+            String pChoice = scan.nextLine().toLowerCase();
+            System.out.println();
             
-            System.out.println("* "+cpuChoiceList.get(i)); 
-        
-        }
-        
-        System.out.print("-> ");
-        String userText = user.nextLine();
-        
-        while(!(userText.equalsIgnoreCase("rock")
-        ||userText.equalsIgnoreCase("paper")
-        ||userText.equalsIgnoreCase("scissors"))) {
-            
-            System.out.println("That's not a valid option. Please input \"ROCK\", \"SCISSORS\", or \"PAPER\".");
-            userText = user.nextLine();
+            while(!(pChoice.equals("rock") || pChoice.equals("paper") || pChoice.equals("scissors"))) {
 
-        }
-       
-        int randNum = cpuChoice.nextInt(cpuChoiceList.size());
-        int det;
-        
-        System.out.print("\n");
-
-        System.out.println(username+" chose: "+userText);
-        System.out.println("Java chose: "+cpuChoiceList.get(randNum));
-        System.out.print("\n");
-        
-        if(userText.equalsIgnoreCase("rock")) {
-            
-            det = 0;
-            
-            if(randNum>det && randNum==1) {
-                
-                System.out.println(username+" won!");
-                userScore++;
-
-            } else if(randNum>det && randNum==2) {
-                
-                System.out.println("Java won!");
-                javaScore++;
-
-            } else if(randNum==det) {
-                
-                System.out.println("It's a tie!");
-                userScore++;
-                javaScore++;
+                System.out.println("Invalid input. Please try again: ");
+                pChoice = scan.nextLine().toLowerCase();
+                System.out.println();
 
             }
 
-        } else if(userText.equalsIgnoreCase("scissors")) {
-            
-            det = 1;
+            String jChoice_2 = compChoice[jChoice()]; 
+            System.out.println();
 
-            if(randNum<det) {
-                
-                System.out.println("Java won!");
-                javaScore++;
+            System.out.println("Java chose: "+jChoice_2);
 
-            } else if(randNum>det) {
-                
-                System.out.println(username+" won!");
-                userScore++;
+            if(pChoice.equals("rock") && jChoice_2.equals(compChoice[0])) {
 
-            } else if(randNum==det) {
-                
-                System.out.println("It's a tie!");
-                userScore++;
-                javaScore++;
+                System.out.println("Tie!");
+                System.out.println();
 
-            }
-        } else if(userText.equalsIgnoreCase("paper")) {
-            
-            det = 2;
+            } else if(pChoice.equals("rock") && jChoice_2.equals(compChoice[1])) {
 
-            if(randNum<det && randNum==1) {
-                
-                System.out.println("Java won!");
-                javaScore++;
+                System.out.println("Java wins!");
+                System.out.println();
+                jScore++;
 
-            } else if(randNum<det && randNum==0) {
-                
-                System.out.println(username+" won!");
-                userScore++;
+            } else if(pChoice.equals("rock") && jChoice_2.equals(compChoice[2])) {
 
-            } else if(randNum==det) {
-                
-                System.out.println("It's a tie!");
-                userScore++;
-                javaScore++;
+                System.out.println("Player wins!");
+                System.out.println();
+                pScore++;
 
-            }
-        }
-        
-        System.out.println("Score: "+username+" "+userScore+" X "+javaScore+" Java");
-        System.out.print("\n");
+            } else if(pChoice.equals("paper") && jChoice_2.equals(compChoice[0])) {
 
-        System.out.println("Want to play again? (Y/N)");
-        System.out.print("-> ");
-        String yesOrNo = user.nextLine();
-        
-        while(!(yesOrNo.equalsIgnoreCase("Yes")
-        ||yesOrNo.equalsIgnoreCase("Y")
-        ||yesOrNo.equalsIgnoreCase("No")
-        ||yesOrNo.equalsIgnoreCase("N"))) {
-            
-            System.out.println("That's not a valid option!");
-            System.out.print("\n");
-            System.out.println("Want to play again? (Y/N)");
-            yesOrNo = user.nextLine();
-            
-            if(yesOrNo.equalsIgnoreCase("Yes")
-            ||yesOrNo.equalsIgnoreCase("Y")
-            ||yesOrNo.equalsIgnoreCase("No")
-            ||yesOrNo.equalsIgnoreCase("N")) {
-                
-                break;
+                System.out.println("Player wins!");
+                System.out.println();
+                pScore++;
 
-            }
-        }
-        
-        if(yesOrNo.equalsIgnoreCase("N")||yesOrNo.equalsIgnoreCase("No")) {
-            
-            System.out.println("Game over!");
-            if(userScore>javaScore) {
-                
-                System.out.println(username+" triumphs over Java!");
+            } else if(pChoice.equals("paper") && jChoice_2.equals(compChoice[1])) {
 
-            } else if(userScore==javaScore) {
-                
-                System.out.println(username+" ties with Java!");
+                System.out.println("Tie!");
+                System.out.println();
+
+            } else if(pChoice.equals("paper") && jChoice_2.equals(compChoice[2])) {
+
+                System.out.println("Java wins!");
+                System.out.println();
+                jScore++;
+
+            } else if(pChoice.equals("scissors") && jChoice_2.equals(compChoice[0])) {
+
+                System.out.println("Java wins!");
+                System.out.println();
+                jScore++;
+
+            } else if(pChoice.equals("scissors") && jChoice_2.equals(compChoice[1])) {
+
+                System.out.println("Player wins!");
+                System.out.println();
+                pScore++;
+
+            } else if(pChoice.equals("scissors") && jChoice_2.equals(compChoice[2])) {
+
+                System.out.println("Tie!");
+                System.out.println();
 
             } else {
-                
-                System.out.println("Java triumphs over "+username+"!");
+
+                System.out.println("default");
 
             }
-            
-            break;
+
+            System.out.print("\nPlayer score: "+pScore+" \nJava score: "+jScore);
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("Want to play again? (Y/N)");
+            String pRepeat = scan.nextLine().toLowerCase();
+            System.out.println();
+
+            while(!(pRepeat.equals("y") || pRepeat.equals("n"))) {
+
+                System.out.println("Invalid input. Please try again: ");
+                pRepeat = scan.nextLine().toLowerCase();
+                System.out.println();
+
+            }
+
+            if(pRepeat.equals("n")) {
+
+                break;
+
+            }
 
         }
-    }
-    
-    user.close();
 
-    } 
+        if(pScore > jScore) {
+
+            System.out.println("Player won the battle!");
+
+        } else if(pScore < jScore) {
+
+            System.out.println("Java won the battle!");
+
+        } else if(pScore == jScore) {
+
+            System.out.println("It's a tie!");
+            
+        }
+
+        scan.close();
+
+    }
+
 }
